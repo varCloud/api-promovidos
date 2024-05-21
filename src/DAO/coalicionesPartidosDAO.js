78b91a59192aa76bb36e54957d49e3f9a131900a
Miguel Mendoza <angelmendoza1451@gmail.com>
2024-05-21 12:52:13 -0600

    cambiando get de coaliciones

--- src/DAO/coalicionesPartidosDAO.js
+++ src/DAO/coalicionesPartidosDAO.js
@@ -1,31 +1,62 @@
 const coalicionesPartidos = require('../Models/coaliconesPartidos.model')
+const Partido = require('../Models/catPartidos.model')
 const { Op } = require('sequelize')
 
 class coalicionesPartidosDAO {
 
   async obtenerCoalicionesPartidos(params) {
     try {
-      let options = params.id
-        ? {
-          [Op.eq]: params.id,
-        }
-        : {
-          [Op.notIn]: 0,
-        };
-      let filter = { idCoalicionPartido: options, activo: 1 };
-      const coalicionPartidoActual = await coalicionesPartidos.findAll({
-        order: [["idCoalicionPartido", "Desc"]],
-        where: filter,
-        include: [
-          {
-            association: "Partidos",
+      const coaliciones = await coalicionesPartidos.findAll({
+        attributes: ['idCoalicion'],
+        group: ['idCoalicion']
+      });
+
+      // Iterar sobre cada coalición y encontrar sus partidos asociados
+      const partidosPorCoalicion = await Promise.all(coaliciones.map(async (coalicion) => {
+        const idCoalicion = coalicion.idCoalicion;
+
+        // Encontrar todos los partidos asociados a esta coalición
+        const partidos = await coalicionesPartidos.findAll({
+          where: {
+            idCoalicion: idCoalicion
           },
+          include: [{
+            model: Partido,
+            required: true
+          }
+            ,
           {
             association: "Coaliciones",
-          },
-        ],
-      });
-      return coalicionPartidoActual;
+          },]
+        });
+        // Construir objeto de salida para esta coalición
+        return {
+          ...partidos[0].Coaliciones.dataValues,
+          partidos: partidos.map(partido => partido.catpartido)
+        };
+      }));
+      return partidosPorCoalicion
+      // let options = params.id
+      //   ? {
+      //     [Op.eq]: params.id,
+      //   }
+      //   : {
+      //     [Op.notIn]: 0,
+      //   };
+      // let filter = { idCoalicionPartido: options, activo: 1 };
+      // const coalicionPartidoActual = await coalicionesPartidos.findAll({
+      //   order: [["idCoalicionPartido", "Desc"]],
+      //   where: filter,
+      //   include: [
+      //     {
+      //       association: "Partidos",
+      //     },
+      //     {
+      //       association: "Coaliciones",
+      //     },
+      //   ],
+      // });
+      // return coalicionPartidoActual;
     } catch (error) {
       throw error;
     }
